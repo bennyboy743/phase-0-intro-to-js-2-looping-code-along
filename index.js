a812dbf0c4d4d5bb17394e881a4f158733bac0f7
Benjamin <benjaminrowe0@gmail.com>
2021-12-28 12:47:51 +1100

    have fixed the problem

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(listOfNames,emotion){
+    let message = []
+    for (let i = 0; i < listOfNames.length; i++) {
+        const element = listOfNames[i];
+        message[i] = `Thank you, ${element}, for the wonderful ${emotion} gift!`
+    }
+    return message
+}
+// console.log(writeCards(names,"surpise"))
+function countDown(number){
+   while(number > 0 -1) {
+       console.log(number)
+       number--
+   }
+}
+console.log(countDown(10))
